241e0eb3e4d04e7c844f0566bc869b0dcfc93a35
muldarof <42444659+Muldarof@users.noreply.github.com>
2025-02-07 20:28:33 -0500

    added a report function

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ def main():
         file_contents = f.read()
         #print(file_contents)
         #print(word_count(file_contents))
+        num_words = word_count(file_contents)
         count = character_counter(file_contents)
-        print(f"{count}")
+        #print(f"{count}")
+        print_report(path_to_file, num_words, count)
 
 def word_count(input):
     words = input.split()
@@ -21,5 +23,13 @@ def character_counter(input_string):
         else:
             result[char] = 1
     return result
+
+def print_report(path_to_file, num_words, count_dict):
+    print(f"--- Begin report of {path_to_file}")
+    print(f"{num_words} words found in the document\n")
+    for entry in count_dict:
+        if entry in "abcdefghijklmnopqrstuvwxyz":
+            print(f"The '{entry}' character was found {count_dict[entry]} times")
+    print("--- End Report ---")
 
 main()
